a82474429a474a665463fc3a35b4e054dba5d5f4
Renato <renatoprata@gmail.com>
2020-03-09 22:34:29 -0300

    criar controller cliente

--- src/main/java/com/itau/cartoes/controller/ClienteController.java
+++ src/main/java/com/itau/cartoes/controller/ClienteController.java
@@ -2,9 +2,14 @@ package com.itau.cartoes.controller;
 
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.itau.cartoes.models.Cliente;
@@ -21,6 +26,12 @@ ClienteService clienteService;
 	public Optional<Cliente> exibeCliente(@PathVariable int id) {
 		return clienteService.buscarCliente(id);
 	}
+	
+	@PostMapping("/cliente")
+	public ResponseEntity<Object> cadastrarCliente(@Valid @RequestBody Cliente cliente) {
+		return clienteService.cadastrar(cliente);
+	}
+	
 	
 	
 }
