9e7bc7f9194cd6ccd272fa16081acc06d1c2821b
Renato <renatoprata@gmail.com>
2020-03-10 21:53:45 -0300

    Ajustes consulta cliente por id

--- src/main/java/com/itau/cartoes/controller/ClienteController.java
+++ src/main/java/com/itau/cartoes/controller/ClienteController.java
@@ -1,7 +1,5 @@
 package com.itau.cartoes.controller;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
